7ca0a81e0cd831932dfe891bfde43208d6500b68
Robert Lockwood Logan IV <rloganiv@gmail.com>
2017-02-22 21:49:08 -0800

    Refactored Model Code + Added PaHaWDataset
    Now all of the data preprocessing is contained in process.py.
    The PaHaWDataset works similarly to the datasets in scikit-learn, with
    added functionality for windowing and subsampling data.

--- model.py
+++ model.py
@@ -1,212 +1,29 @@
-"""Trains an LSTM model on the PaHaW dataset and evaluates using cross validation"""
 from keras.models import Sequential
 from keras.layers import Dense, Activation, LSTM
 
-import numpy as np
-import process
-import pdb
+import pickle
+from process import Subject, PaHaWDataset, extract_datasets
+import time
 
+# Try to load pickled data, if it doesnt exist generate train and test dataset
+# from scratch
+try:
+    with open('PaHaW/processed_data.pkl', 'rb') as pkl_file:
+        train, test = pickle.load(pkl_file)
+except:
+    train, test = extract_datasets()
 
-class Splits(object):
-    """ Stores the kfold splits
+# Model definition
+model = Sequential()
+model.add(LSTM(100, input_dim = 6))
+model.add(Dense(1, activation='sigmoid'))
 
-    Attributes:
-        train: list. A list of the training splits
-        test: list. A list of the testing splits
+# Compile and fit model
+model.compile(loss='binary_crossentropy', optimizer='adam',
+              metrics=['accuracy'])
+model.fit(train.x, train.y, validation_data=(test.x, test.y), nb_epoch=3,
+          batch_size=5)
 
-    """
-    train = []
-    test = []
-
-
-def transform_position(data):
-    """Transform position values into diplacement values.
-
-    Args:
-        data: dictionary. Contains our data where the key is the subject. See process.py for more information.
-    """
-    for subject in data.itervalues():
-        for task_data in subject.task.itervalues():
-            # Subtract the previous time point's coordinates
-            task_data[1:, 0:2] -= task_data[0:-1, 0:2]
-            # Set starting coordinates to (0, 0)
-            task_data[0,0:2] = 0
-
-
-def pad_sequence(data, max_len=500):
-    """Pad data so all sequences have uniform length. This is done by adding sequences with zeros. We also
-        add an extra binary feature that is 1 when data is availble at that
-        time and 0 when we are padding.
-        If a time sequence is over max_len, the extra time points are removed.
-
-    Args:
-        data: dictionary. Contains our data where the key is the subject. See process.py for more information.
-        max_len: int. Max length of all time series.
-    """
-    for subject in data.itervalues():
-        for task_key, task_data in subject.task.iteritems():
-            if task_data.shape[0] < max_len:
-                task_data = np.hstack((task_data, np.ones(shape=(task_data.shape[0], 1))))
-                task_data = np.vstack((task_data, np.zeros(shape=(max_len - task_data.shape[0], 8))))
-            else:
-                task_data = task_data[0:max_len,:]
-                task_data = np.hstack((task_data, np.ones(shape=(task_data.shape[0], 1))))
-            subject.task[task_key] = task_data
-
-
-def remove_time(data):
-    """ Removes the time feature from our data since this is the same across the data
-
-    Args:
-        data: dictionary. Contains our data where the key is the subject. See process.py for more information.
-    """
-    for subject in data.itervalues():
-        for task_key, task_data in subject.task.iteritems():
-            task_data = np.delete(task_data, 2, axis=1)
-            subject.task[task_key] = task_data
-
-
-def feature_extraction(data):
-    """Generates features from raw data. This leaves most of the data the same but involves several changes.
-        - Transforming Cartesian coordinate features into delta features (change in position).
-        - Pads sequences to have the same length.
-        - Removes the time feature.
-        See functions for more detail.
-
-    Args:
-        data: dictionary. Contains our data where the key is the subject. See process.py for more information.
-    """
-    transform_position(data)
-    pad_sequence(data, max_len=16071)   # maximum sequence length of data is 16071, hardcoded
-    remove_time(data)
-
-
-def KFold(data, n_splits=10):
-    """Performs a K-Fold split of our data
-
-    Args:
-        data: dictionary. Contains our data where the key is the subject. See process.py for more information.
-
-    Returns:
-        kf: list. Contains a dictionary for each split. Each dictionary has a
-        'train' and 'test' key indicating the
-            subjects of each set.
-    """
-    from copy import copy
-    from math import ceil
-    import random
-
-    subjs = {}
-    for key in data:
-        subjs[key] = data[key].info['PD status']
-    remaining = copy(subjs)
-
-    kf = Splits()
-    for i in xrange(n_splits):
-        train = copy(subjs)
-        if i == n_splits-1:
-            test = list(remaining.keys())
-        else:
-            test = random.sample(remaining, len(data)/n_splits)
-        for key in test:
-            remaining.pop(key, None)
-            train.pop(key, None)
-
-        kf.test.append(test)
-        kf.train.append(list(train.keys()))
-
-    return kf
-
-
-def create_datasets(data, train, test):
-    """Creates the training and testing datasets from the subjects consitituting each set.
-
-    Args:
-        data: dictionary. Contains our data where the key is the subject. See process.py for more information.
-        train: list. A list of subjects in the training set
-        test: list. A list of subjects in the testing set. Subjects in the testing set are mutually exclusive from the
-            training set.
-    Returns:
-        X_train, y_train, X_test, y_test: numpy arrays. Features and predictions for the training and test set.
-    """
-    num_train = 0
-    num_test = 0
-
-    # Get the number of training and testing sequences
-    for subject_id in train:
-        num_train += len(data[subject_id].task)
-    for subject_id in test:
-        num_test += len(data[subject_id].task)
-
-    # Hard-coded train and test sets
-    X_train = np.empty(shape=(num_train, 16071 ,7))
-    y_train = np.empty(shape=(num_train, 1))
-    X_test = np.empty(shape=(num_test, 16071, 7))
-    y_test = np.empty(shape=(num_test, 1))
-
-    # Extract training and test sets from data dictionary
-    for idx, subject_id in enumerate(train):
-        for task_data in data[subject_id].task.itervalues():
-            task_data = task_data.reshape((1, task_data.shape[0], task_data.shape[1]))
-            X_train[idx] = task_data
-            y_train[idx] = data[subject_id].info['PD status']
-
-    for idx, subject_id in enumerate(test):
-        for task_data in data[subject_id].task.itervalues():
-            task_data = task_data.reshape((1, task_data.shape[0], task_data.shape[1]))
-            X_test[idx] = task_data
-            y_test[idx] = data[subject_id].info['PD status']
-
-    return X_train, y_train, X_test, y_test
-
-
-def normalize_data(X_train, X_test):
-    """ Normalizes features in the training set to be in range [0, 1]. Then applies transformation
-        to test set.
-
-    Args:
-        X_train: numpy array. 3D matrix containing our training sequences
-        X_test: numpy array. 3D array containing our test sequences
-    """
-    max_feature_val = np.max(np.max(X_train, axis=1), axis=0) # highest feature values in training set
-    X_train /= max_feature_val
-    X_test /= max_feature_val
-
-def evaluate_model():
-    """ Loads the data dictionary and then performs K-Fold cross validation to obtain the classification accuracy
-        of our model.
-    """
-    data = process.load_dataset()
-    feature_extraction(data)
-
-    kf_splits = KFold(data)
-    sum_scores = 0
-    for i in xrange(len(kf_splits.train)):
-        print "========= Cross Validation Iteration ", i +1, " out of ", len(kf_splits.train), " ========="
-        # Data Processing and Normalization
-        train = kf_splits.train[i]
-        test = kf_splits.test[i]
-        (X_train, y_train, X_test, y_test) = create_datasets(data, train, test)
-        normalize_data(X_train, X_test)
-
-        # Model Training
-        model = Sequential()
-        model.add(LSTM(100, input_dim=7))
-        model.add(Dense(1, activation='sigmoid'))
-        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-        model.fit(X_train, y_train, validation_data=(X_test, y_test),
-                  nb_epoch=3, batch_size=5)
-
-        # Save model
-        model_json = model.to_json()
-        with open("model_%i.json" % i, "w") as json_file:
-            json_file.write(model_json)
-        # Save weights
-        model.save_weights("model_%i.h5" % i)
-
-    print "========================="
-    accuracy = sum_scores/len(kf_splits.train)
-    print "Cross validation accuracy: ", accuracy
-
-if __name__== '__main__':
-    evaluate_model()
+# Save model
+timestamp = time.strftime("%y-%m-%d_%H-%M")
+model.save('model_%s.h5' % timestamp)
--- process.py
+++ process.py
@@ -1,8 +1,8 @@
 """Process the PaHaW dataset into NumPy arrays"""
-
 import numpy as np
 import pandas as pd
 import pickle
+
 
 # Constants
 CORPUS_PATH = 'PaHaW/PaHaW_files/corpus_PaHaW.xlsx'
@@ -21,6 +21,99 @@ class Subject(object):
     pass
 
 
+class PaHaWDataset(object):
+    """A dataset object"""
+    def __init__(self, subjects, method=None, subsample_rate=100, window_size=100,
+                 stride=25):
+        self.subjects = subjects
+        self.method = method
+        self.subsample_rate = subsample_rate
+        self.window_size = window_size
+        self.stride = stride
+
+    def subsample(self, task):
+        """Subsample rows from a task"""
+        return task[::self.subsample_rate, :]
+
+    def window(self, task):
+        """Generate windows from a task"""
+        n_windows = (task.shape[0] - self.window_size) // self.stride + 1
+        return [
+            task[(i*self.stride):(i*self.stride + self.window_size), :] for i in xrange(n_windows)
+        ]
+
+    def update(self):
+        from keras.preprocessing import sequence
+        x = []
+        y = []
+
+        # Process the task data according to active method for each subject.
+        # Append results to x and y
+        for subject in self.subjects:
+            if self.method == 'subsample':
+                tasks = [self.subsample(task) for task in subject.task.itervalues()]
+            if self.method == 'window':
+                tasks = []
+                for task in subject.task.itervalues():
+                    tasks += self.window(task)
+            if self.method is None:
+                tasks = subject.task.values()
+            x += tasks
+            y += [subject.info['PD status']] * len(tasks)
+
+        # Replace x and y attributes with updated numpy arrays
+        self.x = sequence.pad_sequences(x)
+        self.y = np.array(y)
+
+    @property
+    def method(self):
+        return self._method
+
+    @method.setter
+    def method(self, method):
+        if method in ['subsample', 'window', None]:
+            self._method = method
+            self.update()
+        else:
+            raise ValueError('Invalid method')
+
+    @property
+    def subsample_rate(self):
+        return self._subsample_rate
+
+    @subsample_rate.setter
+    def subsample_rate(self, subsample_rate):
+        if type(subsample_rate) is int:
+            self._subsample_rate = subsample_rate
+            self.update()
+        else:
+            raise ValueError('Subsample rate must be an int')
+
+    @property
+    def window_size(self):
+        return self._window_size
+
+    @window_size.setter
+    def window_size(self, window_size):
+        if type(window_size) is int:
+            self._window_size = window_size
+            self.update()
+        else:
+            raise ValueError('Window size must be an int')
+
+    @property
+    def stride(self):
+        return self._stride
+
+    @stride.setter
+    def stride(self, stride):
+        if type(stride) is int:
+            self._stride = stride
+            self.update()
+        else:
+            raise ValueError('Stride must be an int')
+
+
 def parse_corpus(path):
     """Processes the data in the 'corpus_PaHaW.xlsx' spreadsheet.
 
@@ -30,8 +123,7 @@ def parse_corpus(path):
     Returns:
         pandas.dataframe. Contains the processed spreadsheet data.
     """
-    df = pd.read_excel(path)
-
+    df = pd.read_excel(path) 
     # Replace PD status with a binary variable.
     df['PD status'] = df['PD status'] == 'ON'
     df['PD status'] = df['PD status'].astype(int)
@@ -64,45 +156,58 @@ def parse_svc(path):
 
     Returns:
         np.array. Each row of the array is a sample. The columns represent:
-            0: y-coordinate,
-            1: x-coordinate,
-            2: time stamp,
-            3: button state,
-            4: azimuth,
-            5: altitude,
-            6: pressure
+            0: y-displacement,
+            1: x-displacement,
+            2: button state,
+            3: azimuth,
+            4: altitude,
+            5: pressure
     """
     # Open the file
     with open(path, 'r') as svc_file:
         samples = svc_file.readlines()
 
-    # Process the data
+    # Extract the data
     data = []
     for sample in samples[1:]:
         values = [int(value) for value in sample.split()]
         data.append(values)
-    return np.array(data)
+    array = np.array(data)
+
+    # ---Process the data---
+
+    # x,y coords to displacement
+    array[1:, 0:2] -= array[0:-1, 0:2]
+    array[0, 0:2] = 0
+
+    # remove time stamp
+    array = np.delete(array, 2, axis=1)
+
+    return array
 
 
-def load_dataset():
-    """Builds the PaHaW dataset.
+def extract_datasets(test_fraction = 0.333):
+    """Extracts the train and test datasets.
 
     Returns:
-        dict. A dictionary whose keys are subject id's and whose values are the
-            corresponding Subject objects. See the process.Subject() class
-            docstring for more info.
+        train, test: PaHaWDataset() objects.
     """
-    # Store all subjects in a dictionary
-    dataset = {}
+    import random
+    train_subjects = []
+    test_subjects = []
 
-    # Create a subject for each row in the corpus
+    # Extract data from corpus
     corpus = parse_corpus(CORPUS_PATH)
+
+    # Build a Subject object for each row in the corpus and randomly assign to
+    # train or test dataset
     for row in corpus.iterrows():
         subject = Subject()
         subject.info = row[1]
         subject.task = dict()
         # ID must be converted from int to fixed length string.
         subject_id = '%05d' % row[1].ID
+        # Extract task data from SVC files
         for i in xrange(1, 9):
             try:
                 svc_path = generate_svc_path(subject_id, i)
@@ -110,7 +215,19 @@ def load_dataset():
                 subject.task[i] = task_data
             except IOError:
                 print 'Subject %s did not perform task %i' % (subject_id, i)
-        dataset[subject_id] = subject
+        if random.random() < test_fraction:
+            test_subjects.append(subject)
+        else:
+            train_subjects.append(subject)
+    # Load subjects into datasets
+    train = PaHaWDataset(train_subjects)
+    test = PaHaWDataset(test_subjects)
 
-    return dataset
+    return train, test
+
+
+if __name__ == '__main__':
+    train, test  = extract_datasets()
+    with open('PaHaW/processed_data.pkl', 'wb') as pkl_file:
+        pickle.dump((train, test), pkl_file)
 
